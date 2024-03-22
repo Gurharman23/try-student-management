03cf373399bc322b6203bc7f4c628049bcb2a157
Gurharmanjit Singh <gurharmanjit.singh@jungleworks.com>
2024-03-22 22:47:35 +0530

    integrating babel transpiler for jsx to js transpiling and other es6 to es5 transformation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
+import React from "react";
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
-// root.render(<h1>Hello, world</h1>);
+root.render(<h1>Hello, world</h1>);
