743ea094796dcd5048c89fce33f57f9cd20a174d
Gurharmanjit Singh <gurharmanjit.singh@jungleworks.com>
2024-03-24 13:38:14 +0530

    added HtmlWebpackPlugin, dynamically generated bundles would be added automatically to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -12,6 +13,12 @@ module.exports = {
     hot: true,
     port: 3000,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "index.html",
+    }),
+  ],
   module: {
     rules: [
       {
